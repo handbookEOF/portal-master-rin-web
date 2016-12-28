2a8d4e062404856420705e15b04dc9339be20d0c
Fachri <fachri.ikh1213@gmail.com>
2016-12-28 09:56:06 +0700

    RIN Web Portal

---
+++ contents.js
@@ -0,0 +1,40 @@
+  // Default Value for Home
+  $(".content-box").html(function(){
+    $(".content-body").load("home.html");
+  });
+
+  // Home Content
+  $("#home").on('click', function(){
+    $(".content-box").html(function(){
+      $(".content-body").load("home.html");
+    });
+  });
+
+  // Services Content
+  $("#services").on('click', function(){
+    $(".content-box").html(function(){
+      $(".content-body").load("service.html");
+    });
+  }); 
+
+  // Portfolio Content
+  $("#portfolio").on('click', function(){
+    $(".content-box").html(function(){
+      $(".content-body").load("portfolio.html");
+    });
+  }); 
+
+  // About Content
+  $("#about").on('click', function(){
+    $(".content-box").html(function(){
+      $(".content-body").load("about.html");
+    });
+  }); 
+
+  // Contacts Content
+  $("#contact").on('click', function(){
+    $(".content-box").html(function(){
+      $(".content-body").load("contact.html");
+    });
+  }); 
+
