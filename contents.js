06dd9bc2760ec6c688fe9a8c2f82c72a3dd1a220
Fachri <fachri.ikh1213@gmail.com>
2017-01-02 20:54:43 +0700

    Alpha Version

--- contents.js
+++ contents.js
@@ -1,47 +1,57 @@
-  $.ajax({
-    cache: false
-  })
-  .done(function( html ) {
-    $( "#results" ).append( html );
-  });
+var homePage = "home.html";
+var servicePage = "service.html";
+var portfPage = "portfolio.html";
+var aboutPage = "about.html";
+var contactPage = "contact.html";
 
-  // Default Value for Home
+
+$.ajax({
+   cache: false
+});
+
+/* if($(".content-body").width() > $(".content-box").width()){
+    $(".content-body").css("overflow", "scroll");
+  } else {
+    $(".content-box").css("overflow", "hidden");
+} */
+
+// Default Value for Home
+$(".content-box").html(function(){
+  $(".content-body").load(homePage);
+});
+
+// Home Content
+$("#home").on('click', function(){
   $(".content-box").html(function(){
-    $(".content-body").load("home.html");
+    $(".content-body").load(homePage);
   });
+});
 
-  // Home Content
-  $("#home").on('click', function(){
-    $(".content-box").html(function(){
-      $(".content-body").load("home.html");
-    });
+// Services Content
+$("#services").on('click', function(){
+  $(".content-box").html(function(){
+    $(".content-body").load(servicePage);
   });
+}); 
 
-  // Services Content
-  $("#services").on('click', function(){
-    $(".content-box").html(function(){
-      $(".content-body").load("service.html");
-    });
-  }); 
-
-  // Portfolio Content
-  $("#portfolio").on('click', function(){
-    $(".content-box").html(function(){
-      $(".content-body").load("portfolio.html");
-    });
+// Portfolio Content
+$("#portfolio").on('click', function(){
+  $(".content-box").html(function(){
+    $(".content-body").load(portfPage);
   });
+});
 
-  // About Content
-  $("#about").on('click', function(){
-    $(".content-box").html(function(){
-      $(".content-body").load("about.html");
-    });
-  }); 
+// About Content
+$("#about").on('click', function(){
+  $(".content-box").html(function(){
+    $(".content-body").load(aboutPage);
+  });
+}); 
 
-  // Contacts Content
-  $("#contact").on('click', function(){
-    $(".content-box").html(function(){
-      $(".content-body").load("contact.html");
-    });
-  }); 
+// Contacts Content
+$("#contact").on('click', function(){
+  $(".content-box").html(function(){
+    $(".content-body").load(contactPage);
+  });
+}); 
 
